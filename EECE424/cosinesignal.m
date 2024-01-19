33b9cd1299d3bd13c85be6d23d63f0b66225e5c9
Chidinma Dike <Chidinma.dike000@gmail.com>
2024-01-19 15:32:02 -0500

    Add a clear, smapling, and changed time vector

--- EECE424/cosinesignal.m
+++ EECE424/cosinesignal.m
@@ -1,13 +1,19 @@
 % By Chidinma Dike
-% Define signal parameters
+
+%% clear everything
+clear, close all, clc;
+
+%% Define signal parameters
 frequency = 100e6;  % Frequency in Hz (100 MHz)
 amplitude = 10;    % Amplitude in volts (10 V)
 duration = 1e-6;   % Duration in seconds (1 us)
+over_sampling= 20;
 
 % Generate time vector
-time = linspace(0, duration, 1000);  % Create a time vector with 50 points
+fs =over_smapling*max(frequency);
+time= 0:1/fs: duration-1/fs;
 
-% Generate the cosine signal
+%% Generate the cosine signal
 cosine_signal = amplitude * cos(2*pi*frequency*time);
 
 % Plot the signal
